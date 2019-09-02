7453dd5646a8acec18300bc1701463b8466f414e
Christopher Johnstone <meson800@gmail.com>
2019-09-01 22:25:51 -0400

    Added MesonMessagingService

---
+++ MesonTrading/app/src/main/java/us/meson/mesontrading/MesonTradingMessagingService.kt
@@ -0,0 +1,10 @@
+package us.meson.mesontrading
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MesonTradingMessagingService : FirebaseMessagingService() {
+
+}
